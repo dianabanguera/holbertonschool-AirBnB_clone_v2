a8955cdb6b1d96b10f59eda0d000034f057a906b
dianabanguera <dianabangueravillalobo@gmail.com>
2022-09-13 16:24:45 -0500

    3. Python is cool!

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    return 'Hellos HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def index1():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def index2(text='santi'):
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text='is cool'):
+    """display “Python ”, followed by the value of the text variable"""
+    return 'Python ' + text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')

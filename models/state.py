9432543fe34cae18031e14c95c390d40110ab1a9
dianabanguera <dianabangueravillalobo@gmail.com>
2022-09-13 17:12:16 -0500

    update state

--- models/state.py
+++ models/state.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
-from models.base_model import Base
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.city import City
 from os import getenv
-import models
 
 
-HBNB_TYPE_STORAGE = getenv('HBNB_TYPE_STORAGE')
-
-
-class State(BaseModel, Base):
+class State(BaseModel, Base if (getenv("HBNB_TYPE_STORAGE")=="db") else object):
     """ State class """
-    __tablename__ = 'states'
-    if HBNB_TYPE_STORAGE == 'db':
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = 'states'
         name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state", cascade="delete")
+
     else:
         name = ""
 
     @property
     def cities(self):
-        """getter for list of city instances related to the state"""
-        city_list = []
-        all_cities = models.storage.all(City)
-        for city in all_cities.values():
-            if city.state_id == self.id:
-                city_list.append(city)
-        return city_list
+        """
+        Getter attribute that returns the list of City
+        instances with state_id equals to the current State.id
+        """
+        from models import storage
+        result = []
+        cities = storage.all(City).values()
+        for city in cities:
+            if self.id == city.state_id:
+                result.append(city)
+        return(result)
